446d64840777a6edba71a92394f1eceb506fc9ab
zdf1840 <534404129@qq.com>
2020-12-13 12:37:55 +0800

    微调顶部装饰样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,7 +5,7 @@
       <div v-else class="host-body">
         <el-row style="margin-bottom: 10px">
           <div class="d-flex jc-center">
-            <dv-decoration-10 style="width:33.3%;height:.0625rem; transform: rotateY(180deg);" />
+            <dv-decoration-10 style="width:33.3%;height:.0625rem;" />
             <div class="d-flex jc-center">
               <dv-decoration-8 :color="['#568aea', '#000000']" style="width:2.5rem;height:.625rem;" />
               <div class="title">
